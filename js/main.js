59de9b77fc8d217bf2bef191b8d7210e3c96e417
Francisco Puga <fpuga@icarto.es>
2015-12-19 18:55:22 +0100

    Retrieve data from cartodb in different order

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@ $( document ).ready(function() {
     $('#region-btns button').prop('disabled', true);
     $('#threshold-btns button').prop('disabled', true);
     
-    var sql_statement='SELECT \'España\' as region, sum(seats) as seats, sum(abstention) as abstention, sum(amaiur) as amaiur, sum(blank_votes) as "blank votes", sum(bng) as bng, sum(cc) as cc, sum(ciu) as ciu, sum(compromis) as compromis, sum(eb) as eb, sum(equo) as equo, sum(esquerra) as esquerra, sum(fac) as fac, sum(gbai) as gbai, sum(invalid_votes) as "invalid votes", sum(iu) as iu, sum(others) as others, sum(pa) as pa, sum(pacma) as pacma, sum(pnv) as pnv, sum(pp) as pp, sum(prc) as prc, sum(psoe) as psoe, sum(pxc) as pxc, sum(total_votes) as "total votes", sum(upyd) as upyd FROM tabla_votos'
+    var sql_statement='SELECT \'España\' as region, sum(seats) as seats, sum(total_votes) as "total votes", sum(abstention) as abstention, sum(invalid_votes) as "invalid votes", sum(blank_votes) as "blank votes", sum(pp) as pp, sum(psoe) as psoe, sum(ciu) as ciu, sum(iu) as iu, sum(amaiur) as amaiur,  sum(upyd) as upyd, sum(pnv) as pnv, sum(esquerra) as esquerra, sum(bng) as bng, sum(cc) as cc,  sum(compromis) as compromis, sum(fac) as fac, sum(gbai) as gbai, sum(equo) as equo, sum(pacma) as pacma, sum(eb) as eb, sum(pa) as pa, sum(pxc) as pxc, sum(others) as others  FROM tabla_votos'
     $.getJSON('https://icarto.cartodb.com/api/v2/sql/?q='+sql_statement, function(data) {
         votesByCountry = data.rows;
     });
 
-    sql_statement='SELECT ccaa as region, sum(seats) as seats, sum(abstention) as abstention, sum(amaiur) as amaiur, sum(blank_votes) as "blank votes", sum(bng) as bng, sum(cc) as cc, sum(ciu) as ciu, sum(compromis) as compromis, sum(eb) as eb, sum(equo) as equo, sum(esquerra) as esquerra, sum(fac) as fac, sum(gbai) as gbai, sum(invalid_votes) as "invalid votes", sum(iu) as iu, sum(others) as others, sum(pa) as pa, sum(pacma) as pacma, sum(pnv) as pnv, sum(pp) as pp, sum(prc) as prc, sum(psoe) as psoe, sum(pxc) as pxc, sum(total_votes) as "total votes", sum(upyd) as upyd FROM tabla_votos GROUP BY ccaa'
+    sql_statement='SELECT ccaa as region, sum(seats) as seats, sum(total_votes) as "total votes", sum(abstention) as abstention, sum(invalid_votes) as "invalid votes", sum(blank_votes) as "blank votes", sum(pp) as pp, sum(psoe) as psoe, sum(ciu) as ciu, sum(iu) as iu, sum(amaiur) as amaiur,  sum(upyd) as upyd, sum(pnv) as pnv, sum(esquerra) as esquerra, sum(bng) as bng, sum(cc) as cc,  sum(compromis) as compromis, sum(fac) as fac, sum(gbai) as gbai, sum(equo) as equo, sum(pacma) as pacma, sum(eb) as eb, sum(pa) as pa, sum(pxc) as pxc, sum(others) as others  FROM tabla_votos GROUP BY ccaa'
     $.getJSON('https://icarto.cartodb.com/api/v2/sql/?q='+sql_statement, function(data) {
         votesByCCAA = data.rows;
     });
     
-    sql_statement='SELECT province as region, seats as seats, abstention as abstention, amaiur as amaiur, blank_votes as "blank votes", bng as bng, cc as cc, ciu as ciu, compromis as compromis, eb as eb, equo as equo, esquerra as esquerra, fac as fac, gbai as gbai, invalid_votes as "invalid votes", iu as iu, others as others, pa as pa, pacma as pacma, pnv as pnv, pp as pp, prc as prc, psoe as psoe, pxc as pxc, total_votes as "total votes", upyd as upyd FROM tabla_votos'
+    sql_statement='SELECT province as region, seats as seats, total_votes as "total votes", abstention as abstention, invalid_votes as "invalid votes", blank_votes as "blank votes", pp as pp, psoe as psoe, ciu as ciu, iu as iu, amaiur as amaiur, upyd as upyd, pnv as pnv, esquerra as esquerra, bng as bng, cc as cc,  compromis as compromis, fac as fac, gbai as gbai, equo as equo, pacma as pacma, eb as eb, pa as pa, pxc as pxc, others as others  FROM tabla_votos'
     $.getJSON('https://icarto.cartodb.com/api/v2/sql/?q='+sql_statement, function(data) {
         votesByProvince = data.rows;
     });
