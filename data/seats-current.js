0a0f53f9a85b2374d2ce1117310ed520bd852a0a
Andrés Maneiro <andres.maneiro@gmail.com>
2015-12-19 12:25:54 +0100

    seats variation + jqueryze code

---
+++ data/seats-current.js
@@ -0,0 +1,15 @@
+var seatsCurrent = {
+  "pp":      186,
+  "psoe":    110,
+  "ciu":      16,
+  "iu":       11,
+  "amaiur":    7,
+  "upyd":      5,
+  "pnv":       5,
+  "esquerra":  3,
+  "bng":       2,
+  "cc":        2,
+  "compromis": 1,
+  "fac":       1,
+  "gbai":      1
+};
--- js/functions.js
+++ js/functions.js
@@ -1,110 +1,132 @@
 function updateThresholdTo0(){
   console.log('update threshold to 0');
-  document.getElementById('threshold-0').className = 'active btn btn-default';
-  document.getElementById('threshold-1').className = 'btn btn-default';
-  document.getElementById('threshold-3').className = 'btn btn-default';
-  document.getElementById('threshold-5').className = 'btn btn-default';
+  $('#threshold-0').removeClass().addClass('active btn btn-default');
+  $('#threshold-1').removeClass().addClass('btn btn-default');
+  $('#threshold-3').removeClass().addClass('btn btn-default');
+  $('#threshold-5').removeClass().addClass('btn btn-default');
   setConfig({'threshold': 0});
 }
 
 function updateThresholdTo1(){
   console.log('update threshold to 1');
-  document.getElementById('threshold-0').className = 'btn btn-default';
-  document.getElementById('threshold-1').className = 'active btn btn-default';
-  document.getElementById('threshold-3').className = 'btn btn-default';
-  document.getElementById('threshold-5').className = 'btn btn-default';
+  $('#threshold-0').removeClass().addClass('btn btn-default');
+  $('#threshold-1').removeClass().addClass('active btn btn-default');
+  $('#threshold-3').removeClass().addClass('btn btn-default');
+  $('#threshold-5').removeClass().addClass('btn btn-default');
   setConfig({'threshold': 1});
 }
 
 function updateThresholdTo3(){
   console.log('update threshold to 3');
-  document.getElementById('threshold-0').className = 'btn btn-default';
-  document.getElementById('threshold-1').className = 'btn btn-default';
-  document.getElementById('threshold-3').className = 'active btn btn-default';
-  document.getElementById('threshold-5').className = 'btn btn-default';
+  $('#threshold-0').removeClass().addClass('btn btn-default');
+  $('#threshold-1').removeClass().addClass('btn btn-default');
+  $('#threshold-3').removeClass().addClass('active btn btn-default');
+  $('#threshold-5').removeClass().addClass('btn btn-default');
   setConfig({'threshold': 3});
 }
 
 function updateThresholdTo5(){
   console.log('update threshold to 5');
-  document.getElementById('threshold-0').className = 'btn btn-default';
-  document.getElementById('threshold-1').className = 'btn btn-default';
-  document.getElementById('threshold-3').className = 'btn btn-default';
-  document.getElementById('threshold-5').className = 'active btn btn-default';
+  $('#threshold-0').removeClass().addClass('btn btn-default');
+  $('#threshold-1').removeClass().addClass('btn btn-default');
+  $('#threshold-3').removeClass().addClass('btn btn-default');
+  $('#threshold-5').removeClass().addClass('active btn btn-default');
   setConfig({'threshold': 5});
 }
 
 function updateRegionToProvince(){
   console.log('update region to province');
-  document.getElementById('region-province').className = 'active btn btn-default';
-  document.getElementById('region-ccaa').className = 'btn btn-default';
-  document.getElementById('region-spain').className = 'btn btn-default';
+  $('#region-province').removeClass().addClass('active btn btn-default');
+  $('#region-ccaa').removeClass().addClass('btn btn-default');
+  $('#region-country').removeClass().addClass('btn btn-default');
   setConfig({'groupBy':'province'});
 }
 
 function updateRegionToCCAA(){
   console.log('update region to CCAA');
-  document.getElementById('region-province').className = 'btn btn-default';
-  document.getElementById('region-ccaa').className = 'active btn btn-default';
-  document.getElementById('region-spain').className = 'btn btn-default';
+  $('#region-province').removeClass().addClass('btn btn-default');
+  $('#region-ccaa').removeClass().addClass('active btn btn-default');
+  $('#region-country').removeClass().addClass('btn btn-default');
   setConfig({'groupBy':'ccaa'});
 }
 
 function updateRegionToCountry(){
   console.log('update region to CCAA');
-  document.getElementById('region-province').className = 'btn btn-default';
-  document.getElementById('region-ccaa').className = 'btn btn-default';
-  document.getElementById('region-spain').className = 'active btn btn-default';
+  $('#region-province').removeClass().addClass('btn btn-default');
+  $('#region-ccaa').removeClass().addClass('btn btn-default');
+  $('#region-country').removeClass().addClass('active btn btn-default');
   setConfig({'groupBy': 'country'});
 }
 
 function updateViz(seats, proportion){
 
-  // seats
+  updateTable(seats, proportion);
+  createRects(seats);
+  // TODO: update map
+}
+
+function updateSeats(seatsNew, seatsCurrent, seatsEl, seatsVarEl){
+  var seatsVariation = seatsNew - seatsCurrent;
+  seatsEl.html(seatsNew);
+  seatsVarEl.html(seatsVariation);
+
+  // style variation badge
+  seatsVarEl.removeClass('badge up down equal');
+  if (seatsVariation > 0) {
+    seatsVarEl.addClass('badge up') ;
+  } else if(seatsVariation < 0){
+    seatsVarEl.addClass('badge down');
+  } else{
+    seatsVarEl.addClass('equal');
+    seatsVarEl.html('-'); // - is more concise than 0
+  }
+}
+
+function updateTable(seats, proportion){
+
   // TODO: update color badges
   // TODO: include parties that would have a seat in other scenarios
-  document.getElementById('seats-pp').textContent = seats[0]["pp"] || 0;
-  document.getElementById('seats-psoe').textContent = seats[0]["psoe"] || 0;
-  document.getElementById('seats-ciu').textContent = seats[0]["ciu"] || 0;
-  document.getElementById('seats-iu').textContent = seats[0]["iu"] || 0;
-  document.getElementById('seats-amaiur').textContent = seats[0]["amaiur"] || 0;
-  document.getElementById('seats-upyd').textContent = seats[0]["upyd"] || 0;
-  document.getElementById('seats-pnv').textContent = seats[0]["pnv"] || 0;
-  document.getElementById('seats-esquerra').textContent = seats[0]["esquerra"] || 0;
-  document.getElementById('seats-bng').textContent = seats[0]["bng"] || 0;
-  document.getElementById('seats-cc').textContent = seats[0]["cc"] || 0;
-  document.getElementById('seats-compromis').textContent = seats[0]["compromis"] || 0;
-  document.getElementById('seats-fac').textContent = seats[0]["fac"] || 0;
-  document.getElementById('seats-gbai').textContent = seats[0]["gbai"] || 0;
-  document.getElementById('seats-equo').textContent = seats[0]["equo"] || 0;
-  document.getElementById('seats-pacma').textContent = seats[0]["pacma"] || 0;
-  document.getElementById('seats-eb').textContent = seats[0]["eb"] || 0;
-  document.getElementById('seats-pa').textContent = seats[0]["pa"] || 0;
+  updateSeats(seats["pp"] || 0, seatsCurrent["pp"] || 0, $('#seats-pp'), $('#seats-var-pp'));
+  updateSeats(seats["psoe"] || 0, seatsCurrent["psoe"] || 0, $('#seats-psoe'), $('#seats-var-psoe'));
+  updateSeats(seats["ciu"] || 0, seatsCurrent["ciu"] || 0, $('#seats-ciu'), $('#seats-var-ciu'));
+  updateSeats(seats["iu"] || 0, seatsCurrent["iu"] || 0, $('#seats-iu'), $('#seats-var-iu'));
+  updateSeats(seats["amaiur"] || 0, seatsCurrent["amaiur"] || 0, $('#seats-amaiur'), $('#seats-var-amaiur'));
+  updateSeats(seats["upyd"] || 0, seatsCurrent["upyd"] || 0, $('#seats-upyd'), $('#seats-var-upyd'));
+  updateSeats(seats["pnv"] || 0, seatsCurrent["pnv"] || 0, $('#seats-pnv'), $('#seats-var-pnv'));
+  updateSeats(seats["esquerra"] || 0, seatsCurrent["esquerra"] || 0, $('#seats-esquerra'), $('#seats-var-esquerra'));
+  updateSeats(seats["bng"] || 0, seatsCurrent["bng"] || 0, $('#seats-bng'), $('#seats-var-bng'));
+  updateSeats(seats["cc"] || 0, seatsCurrent["cc"] || 0, $('#seats-cc'), $('#seats-var-cc'));
+  updateSeats(seats["compromis"] || 0, seatsCurrent["compromis"] || 0, $('#seats-compromis'), $('#seats-var-compromis'));
+  updateSeats(seats["fac"] || 0, seatsCurrent["fac"] || 0, $('#seats-fac'), $('#seats-var-fac'));
+  updateSeats(seats["gbai"] || 0, seatsCurrent["gbai"] || 0, $('#seats-gbai'), $('#seats-var-gbai'));
+  updateSeats(seats["equo"] || 0, seatsCurrent["equo"] || 0, $('#seats-equo'), $('#seats-var-equo'));
+  updateSeats(seats["pacma"] || 0, seatsCurrent["pacma"] || 0, $('#seats-pacma'), $('#seats-var-pacma'));
+  updateSeats(seats["eb"] || 0, seatsCurrent["eb"] || 0, $('#seats-eb'), $('#seats-var-eb'));
+  updateSeats(seats["pa"] || 0, seatsCurrent["pa"] || 0, $('#seats-pa'), $('#seats-var-pa'));
+  updateSeats(seats["others"] || 0, seatsCurrent["others"] || 0, $('#seats-others'), $('#seats-var-others'));
 
   // proportion
   // TODO: update color badges
   // TODO: recalculate % from votes
   // TODO: include parties that would have a seat in other scenarios
-  document.getElementById('proportion-pp').textContent = proportion["pp"] || '-';
-  document.getElementById('proportion-psoe').textContent = proportion["psoe"] || '-';
-  document.getElementById('proportion-ciu').textContent = proportion["ciu"] || '-';
-  document.getElementById('proportion-iu').textContent = proportion["iu"] || '-';
-  document.getElementById('proportion-amaiur').textContent = proportion["amaiur"] || '-';
-  document.getElementById('proportion-upyd').textContent = proportion["upyd"] || '-';
-  document.getElementById('proportion-pnv').textContent = proportion["pnv"] || '-';
-  document.getElementById('proportion-esquerra').textContent = proportion["esquerra"] || '-';
-  document.getElementById('proportion-bng').textContent = proportion["bng"] || '-';
-  document.getElementById('proportion-cc').textContent = proportion["cc"] || '-';
-  document.getElementById('proportion-compromis').textContent = proportion["compromis"] || '-';
-  document.getElementById('proportion-fac').textContent = proportion["fac"] || '-';
-  document.getElementById('proportion-gbai').textContent = proportion["gbai"] || '-';
-  document.getElementById('proportion-equo').textContent = proportion["equo"] || '-';
-  document.getElementById('proportion-pacma').textContent = proportion["pacma"] || '-';
-  document.getElementById('proportion-eb').textContent = proportion["eb"] || '-';
-  document.getElementById('proportion-pa').textContent = proportion["pa"] || '-';
-
-  //updateSeatsBar
-  createRects(seats);
+  $('#proportion-pp').html(proportion["pp"] || '-');
+  $('#proportion-psoe').html(proportion["psoe"] || '-');
+  $('#proportion-ciu').html(proportion["ciu"] || '-');
+  $('#proportion-iu').html(proportion["iu"] || '-');
+  $('#proportion-amaiur').html(proportion["amaiur"] || '-');
+  $('#proportion-upyd').html(proportion["upyd"] || '-');
+  $('#proportion-pnv').html(proportion["pnv"] || '-');
+  $('#proportion-esquerra').html(proportion["esquerra"] || '-');
+  $('#proportion-bng').html(proportion["bng"] || '-');
+  $('#proportion-cc').html(proportion["cc"] || '-');
+  $('#proportion-compromis').html(proportion["compromis"] || '-');
+  $('#proportion-fac').html(proportion["fac"] || '-');
+  $('#proportion-gbai').html(proportion["gbai"] || '-');
+  $('#proportion-equo').html(proportion["equo"] || '-');
+  $('#proportion-pacma').html(proportion["pacma"] || '-');
+  $('#proportion-eb').html(proportion["eb"] || '-');
+  $('#proportion-pa').html(proportion["pa"] || '-');
+  $('#proportion-others').html(proportion["others"] || '-');
 }
 
 var ElectionsDefaultModel = {
@@ -129,40 +151,29 @@ function setConfig(options){
     var votes = votesByCountry;
     var seats = seatsFromVotes(votes);
     var proportion = calculateProportion(votes, seats);
-    updateViz(seats, proportion);
+    updateViz(seats[0], proportion);
   } else if(ElectionsNewModel.groupBy === 'ccaa'){
     var votes = votesByCCAA;
     var seats = seatsFromVotes(votes);
     var proportion = calculateProportion(votes, seats);
-    updateViz(seats, proportion);
+    updateViz(seats[0], proportion);
   } else if (ElectionsNewModel.groupBy === 'province'){
     var votes = votesByProvince;
-    // TODO: make it as the others
-    // var seats = seatFromVotes(votesByProvince);
     var seats = seatsByProvince();
     var proportion = calculateProportion(votes, seats);
     return updateViz(seats, proportion);
+    // TODO: make it as the others
+    // var votes = votesByProvince;
+    // var seats = seatsFromVotes(votes);
+    // var proportion = calculateProportion(votes, seats);
+    //return updateViz(seats[0], proportion);
   }
   // TODO: include province in proper flux
 
 }
 
 function seatsByProvince(){
-  return [{
-    "pp":      186,
-    "psoe":    110,
-    "ciu":      16,
-    "iu":       11,
-    "amaiur":    7,
-    "upyd":      5,
-    "pnv":       5,
-    "esquerra":  3,
-    "bng":       2,
-    "cc":        2,
-    "compromis": 1,
-    "fac":       1,
-    "gbai":      1
-  }];
+  return seatsCurrent;
 }
 
 function calculateProportion(){
