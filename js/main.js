65bbc5dd35e57118c03b16ccf9983671c16ee1ce
Andrés Maneiro <andres.maneiro@gmail.com>
2015-12-21 13:00:47 +0100

    update viz only when data is available

--- js/main.js
+++ js/main.js
@@ -4,22 +4,33 @@ $( document ).ready(function() {
 
     $.getJSON('https://icarto.cartodb.com/api/v2/sql/?q='+sqlCountry, function(data) {
         votesByCountry = data.rows;
+        updateViz();
     });
 
     $.getJSON('https://icarto.cartodb.com/api/v2/sql/?q='+sqlCCAA, function(data) {
         votesByCCAA = data.rows;
+        updateViz();
     });
 
     $.getJSON('https://icarto.cartodb.com/api/v2/sql/?q='+sqlProvince, function(data) {
         votesByProvince = data.rows;
-
-        svgNew();
-        svgOld(parties, colors);
-        populateTable();
-
-        $('#region-btns button').prop('disabled', false);
-        $('#threshold-btns button').prop('disabled', false);
+        updateViz();
     });
 
 
 });
+
+var dataCount = 0;
+function updateViz(){
+  dataCount++;
+  if (dataCount === 3){
+    // update viz when all data is received
+    // as we need them to do some calculations
+    svgNew();
+    svgOld(parties, colors);
+    populateTable();
+
+    $('#region-btns button').prop('disabled', false);
+    $('#threshold-btns button').prop('disabled', false);
+  }
+};
